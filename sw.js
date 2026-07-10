e0bbbebca16565e969506b8d3dc82ec7ca684cf2
HERPOEL, Quentin <quentin.herpoel@ceratec.eu>
2026-07-10 17:17:58 +0200

    added TTS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'series-repos-v2';
+const CACHE = 'series-repos-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
